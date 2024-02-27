11461927da7756cbcad60097e2b507634dc254e7
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-02-27 17:29:31 +0100

    mod

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
 
-	newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	newNode = malloc(sizeof(binary_tree_t));
 
 	if (!parent || !newNode)
 		return (NULL);
