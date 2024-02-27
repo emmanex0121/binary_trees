a1ae9ad1699a5640b5b89fac1d226194a71eb4eb
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-02-27 20:32:19 +0100

    height of tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,18 +11,18 @@
 size_t find_height(const binary_tree_t *tree)
 {
 	size_t height_left = 0;
-        size_t height_right = 0;
+	size_t height_right = 0;
 
 	if (tree == NULL)
-		return(0);
+		return (0);
 
 	height_left = 1 + find_height(tree->left);
-        height_right = 1 + find_height(tree->right);
+	height_right = 1 + find_height(tree->right);
 
 	if (height_left > height_right)
-                return height_left;
-        else
-                return height_right;
+		return (height_left);
+	else
+		return (height_right);
 }
 
 /**
