3997786710d775fd3a6dc52f5a4f83260983a02b
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-02-27 01:34:01 +0100

    Check if node is root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - check if a node is root
+ * @node: node to check/test
+ *
+ * Return: 1 if node is a root, 0 if node is NULL
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!(node->parent))
+		return (1);
+	else
+		return (0);
+}
