4a575382a5efdac2c591675c2972815dd3e34f1a
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-02-27 17:46:55 +0100

    mod 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,8 +9,8 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node || !(node->left) || !(node->right))
-		return (1);
-	else
+	if (!node || node->left || node->right)
 		return (0);
+	else
+		return (1);
 }
