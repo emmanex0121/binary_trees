5864f9ffbdbfb23340b4ae6958901a7ab101801f
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-02-28 02:29:58 +0100

    checks if a tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -36,7 +36,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+		return (1);
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
 
