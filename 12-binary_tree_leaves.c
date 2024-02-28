04a64b9ddc77946e40d28bff221e974db90e0226
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-02-28 01:09:18 +0100

    counts leaves of a node

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Function that counts leaves of a tree
+ * @tree: root tree to be counted
+ *
+ * Return: Number of leaves on success OR 0 on failure
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+
+	size_t leaf = 0;
+
+	if (!tree)
+		return (0);
+	
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	leaf = leaf + binary_tree_leaves(tree->left);
+        leaf = leaf +  binary_tree_leaves(tree->right);
+
+	return (leaf);
+}
