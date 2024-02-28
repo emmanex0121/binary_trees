456fbb9e21d1e1c9ce0e3953fb3da087ea8fb218
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-02-28 02:38:56 +0100

    checks if a tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,23 +1,5 @@
 #include "binary_trees.h"
-
-/**
- * binary_tree_size - counts all the nodes in a tree
- * @tree: Tree to be counted
- *
- * Return: size of tree on success, OR 0 if tree is NULL
- */
-
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	size_t size = 0;
-
-	if (!tree)
-		return (0);
-
-	size = size + binary_tree_size(tree->left);
-	size = size + binary_tree_size(tree->right);
-	return (size + 1);
-}
+#include "11-binary_tree_size.c"
 
 /**
  * binary_tree_is_full - checks if tree is full
@@ -39,8 +21,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
 
-	left_branch = binary_tree_size(tree->left);
-	right_branch = binary_tree_size(tree->right);
+	if (tree->left && tree->right)
+	{
+		left_branch = binary_tree_size(tree->left);
+		right_branch = binary_tree_size(tree->right);
+	}
 
 	if (left_branch == right_branch)
 		return (1);
