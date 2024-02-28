ed9af20f34cca205880d1498c2dfe72a47ba7d08
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-02-28 02:45:39 +0100

    checks if a tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -25,9 +25,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	{
 		left_branch = binary_tree_size(tree->left);
 		right_branch = binary_tree_size(tree->right);
-
-		if (left_branch == right_branch)
-			return (1);
 	}
+
+	if (left_branch == right_branch)
+		return (1);
 	return (0);
 }
