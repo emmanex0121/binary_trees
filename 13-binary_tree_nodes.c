30ac8de8db80178dff75a7191dbdc444d3ace3e6
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-02-28 01:55:09 +0100

    count nodes with at least 1 child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,6 +1,10 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_nodes - function to count all nodes with at least 1 child
+ * @tree: pointer to root node of tree
+ *
+ * Return: number of nodes with child, OR 0 on failure
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
