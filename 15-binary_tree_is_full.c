4f0f478b91f175ec926040d5b615f1931c3c38ef
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-02-28 03:37:13 +0100

    mod 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -18,8 +18,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	if (tree->left == NULL || tree->right == NULL)
-		return (0);
+	/*if (tree->left == NULL || tree->right == NULL)*/
+		/*return (0);*/
 
 	left_branch = binary_tree_is_full(tree->left);
 	right_branch = binary_tree_is_full(tree->right);
